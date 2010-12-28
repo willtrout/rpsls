becd8ac34004f4d117a0de854d8d24b5fc224bc9
Matt Willtrout <willtrout@troutlinetx.com>
2010-12-27 23:15:46 -0600

    use strings instead of char arrays

--- src/rpsls.cpp
+++ src/rpsls.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-  char guess[9];
+  string guess;
   cout << "Rock, Paper, Scissors, Lizard, spocK?";
   cin >> guess;
-  cout << "You chose : ";
-  int index=0;
-  while(guess[index]!='\0')
-  {
-    cout << guess[index];
-    index++;
-  }
-  cout << endl;
+  cout << "You chose : " << guess << endl;
   return 0;
 }
