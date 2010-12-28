c407d22a5fc3a606fffa8f39416e60e328c7c4a2
Matt Willtrout <willtrout@troutlinetx.com>
2010-12-27 23:17:23 -0600

    whitespace twiddle

--- src/rpsls.cpp
+++ src/rpsls.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char *argv[]){
   string guess;
-  cout << "Rock, Paper, Scissors, Lizard, spocK?";
+  cout << "Rock, Paper, Scissors, Lizard, spocK? ";
   cin >> guess;
   cout << "You chose : " << guess << endl;
   return 0;
