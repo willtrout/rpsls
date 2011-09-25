22fa19a6f189aa133da24ab369ab4733a09b21f3
Matthew Willtrout <willtrout@troutlinetx.com>
2011-09-24 20:09:08 -0500

    add computer guess

--- src/rpsls.cpp
+++ src/rpsls.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+
 
 using namespace std;
 
@@ -8,5 +10,28 @@ int main(int argc, char *argv[]){
   cout << "Rock, Paper, Scissors, Lizard, spocK? ";
   cin >> guess;
   cout << "You chose : " << guess << endl;
+  // my turn
+  int dice; string choice;
+  srand ( time(NULL) );
+  dice = rand () % 5 + 1;
+  switch (dice){
+    case 1:
+      choice= "Rock";
+      break;
+    case 2:
+      choice= "Paper";
+      break;
+    case 3:
+      choice= "Scissors";
+      break;
+    case 4:
+      choice= "Lizard";
+      break;
+    case 5:
+      choice= "spocK";
+      break;
+  }
+  cout << "I choose : " << choice << endl;
+  
   return 0;
 }
